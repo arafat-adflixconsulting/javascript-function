b3105bffad3547e03ed7360f28498dc7ad579eb2
arafat-adflixconsulting <arafathossainraj@gmail.com>
2025-08-12 17:25:22 +0600

    checking longest word code is added

--- homework2.js
+++ homework2.js
@@ -11,6 +11,8 @@ function convert(celsius) {
 
 // console.log(celToFahren);
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 //Task-2:
 /* You are given an array of numbers. Count how many times the a number is repeated in the array. */
 
@@ -35,6 +37,7 @@ let arrayNumbers = [5, 6, 11, 12, 98, 5];
 let checkNumber = repeateTime(arrayNumbers, 5);
 // console.log(checkNumber);
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 // Task-3:
 /* Write a function to count the number of vowels in a string. */
 
@@ -49,4 +52,32 @@ function vowels(context) {
 
 let text = "equipment";
 let quantityOfVowels = vowels(text);
-console.log(quantityOfVowels);
+// console.log(quantityOfVowels);
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* Task-4:
+ * Write a function to find the longest word in a given string.
+ * sample-input: I am learning Programming to become a programmer
+ * sample-output: Programming
+ */
+function findLongestWord(sentence) {
+  let words = sentence.split(" "); // বাক্যকে স্পেস দিয়ে ভাগ করা
+  let longest = words[0]; // প্রথম শব্দটাকে ধরা
+
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longest.length) {
+      longest = words[i]; // বড় শব্দ পেলে সেটাকে longest এ রাখবো
+    }
+  }
+
+  return longest;
+}
+
+let mainText = "I am learning Programming to become a programmer";
+console.log(findLongestWord(mainText)); // Output: Programming
+
+/* Generate a random number
+ * between 10 to 20.
+ */
+console.log(Math.random() * 10);
